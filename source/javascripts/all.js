3aa4aab8abc8782bf994c98bc73beb5e817922c4
chundabear <jordan@holyarchers.com>
2015-05-26 20:13:16 -0500

    added leaflet

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,16 @@
 //= require_tree .
+$(window).ready(function(){
+
+  var map = L.map('map').setView([29.591413, -95.459528], 13);
+
+  L.tileLayer('https://api.tiles.mapbox.com/v4/chundabear.m9kd1gli/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2h1bmRhYmVhciIsImEiOiI1d1VyMTRrIn0.VEZKraUkj5Scu6y0urC5DQ', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'chundabear.m9kd1gli',
+      accessToken: 'pk.eyJ1IjoiY2h1bmRhYmVhciIsImEiOiI1d1VyMTRrIn0.VEZKraUkj5Scu6y0urC5DQ'
+  }).addTo(map);
+
+  var marker = L.marker([29.591413, -95.459528]).addTo(map);
+  marker.bindPopup("We meet at Ridgemont Community Center.").openPopup();
+
+});
